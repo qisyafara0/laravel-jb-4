7dffd96d76329c4089b9a3b629a012e49a63fec8
qisyafara0 <namiichawn@gmail.com>
2025-03-11 09:06:31 +0700

    Add File

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container mt-4">
+            <div class="card">
+                <div class="card-header">
+                    <h1> Profil Saya </h1>
+                </div>
+                <div class="card-body">
+                    <img src="" alt="">
+                    <h3> Qisya Farabila </h3>
+                    <p> Email: qisyafarabila@gmail.com</p>
+                    <p> Bio: Murid Sholehah</p>
+                    <h5> Skill: </h5>
+                    <ul>
+                        <li>Mengaji</li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </body>
+</html>
